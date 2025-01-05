a5a1363b8edf9612048dff7106289df776cd9296
Augusto Sanchez <sanchez.augusto.n@gmail.com>
2025-01-04 22:01:56 -0300

    Backend: records files deleted due to names folder changed

--- Backend/api/records/types.ts
+++
@@ -1,9 +0,0 @@
-export interface IRecord {
-    _id: string | undefined;
-    _id_professional: string;
-    _id_patient: string;
-    title: string;
-    file: string;
-    description?: string;
-    date: Date;
-}
